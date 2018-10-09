7764f344aeed57b59f84f1ea13f3ac4c99860695
Phil Wright- Christie <philwc@gmail.com>
2018-10-09 10:40:53 +0100

    Updates

--- precache-manifest.7705b29b4b5c46f6122a1d68f6b6745f.js
+++ precache-manifest.7705b29b4b5c46f6122a1d68f6b6745f.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/github-pr-viewer/static/media/Roboto-Regular.30799efa.eot"
   },
   {
-    "revision": "ba273b65d0e98c3472ab",
-    "url": "/github-pr-viewer/static/js/main.ba273b65.chunk.js"
+    "revision": "bdfd6cebe573142fd2c6",
+    "url": "/github-pr-viewer/static/js/main.bdfd6ceb.chunk.js"
   },
   {
     "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
@@ -16,16 +16,16 @@ self.__precacheManifest = [
     "url": "/github-pr-viewer/static/js/runtime~main.8bfc73fe.js"
   },
   {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/github-pr-viewer/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
     "revision": "674f50d287a8c48dc19ba404d20fe713",
     "url": "/github-pr-viewer/static/media/fontawesome-webfont.674f50d2.eot"
   },
   {
     "revision": "af7ae505a9eed503f8b8e6982036873e",
     "url": "/github-pr-viewer/static/media/fontawesome-webfont.af7ae505.woff2"
-  },
-  {
-    "revision": "fee66e712a8a08eef5805a46892932ad",
-    "url": "/github-pr-viewer/static/media/fontawesome-webfont.fee66e71.woff"
   },
   {
     "revision": "b06871f281fee6b241d60582ae9369b9",
@@ -48,10 +48,6 @@ self.__precacheManifest = [
     "url": "/github-pr-viewer/static/media/Roboto-Thin.7500519d.woff"
   },
   {
-    "revision": "94998475f6aea65f558494802416c1cf",
-    "url": "/github-pr-viewer/static/media/Roboto-Thin.94998475.ttf"
-  },
-  {
     "revision": "a990f611f2305dc12965f186c2ef2690",
     "url": "/github-pr-viewer/static/media/Roboto-Light.a990f611.eot"
   },
@@ -60,12 +56,12 @@ self.__precacheManifest = [
     "url": "/github-pr-viewer/static/media/Roboto-Light.69f8a061.woff2"
   },
   {
-    "revision": "3b813c2ae0d04909a33a18d792912ee7",
-    "url": "/github-pr-viewer/static/media/Roboto-Light.3b813c2a.woff"
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/github-pr-viewer/static/media/Roboto-Thin.94998475.ttf"
   },
   {
-    "revision": "8ef6a08cdc1154920165680a4edde771",
-    "url": "/github-pr-viewer/static/media/arrow_right.8ef6a08c.svg"
+    "revision": "3b813c2ae0d04909a33a18d792912ee7",
+    "url": "/github-pr-viewer/static/media/Roboto-Light.3b813c2a.woff"
   },
   {
     "revision": "46e48ce0628835f68a7369d0254e4283",
@@ -84,10 +80,6 @@ self.__precacheManifest = [
     "url": "/github-pr-viewer/static/media/Roboto-Medium.4d9f3f9e.eot"
   },
   {
-    "revision": "df7b648ce5356ea1ebce435b3459fd60",
-    "url": "/github-pr-viewer/static/media/Roboto-Regular.df7b648c.ttf"
-  },
-  {
     "revision": "574fd0b50367f886d359e8264938fc37",
     "url": "/github-pr-viewer/static/media/Roboto-Medium.574fd0b5.woff2"
   },
@@ -96,12 +88,12 @@ self.__precacheManifest = [
     "url": "/github-pr-viewer/static/media/Roboto-Medium.fc78759e.woff"
   },
   {
-    "revision": "ecdd509cadbf1ea78b8d2e31ec52328c",
-    "url": "/github-pr-viewer/static/media/Roboto-Bold.ecdd509c.eot"
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/github-pr-viewer/static/media/Roboto-Regular.df7b648c.ttf"
   },
   {
-    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
-    "url": "/github-pr-viewer/static/media/Roboto-Medium.894a2ede.ttf"
+    "revision": "ecdd509cadbf1ea78b8d2e31ec52328c",
+    "url": "/github-pr-viewer/static/media/Roboto-Bold.ecdd509c.eot"
   },
   {
     "revision": "39b2c3031be6b4ea96e2e3e95d307814",
@@ -116,6 +108,14 @@ self.__precacheManifest = [
     "url": "/github-pr-viewer/static/media/arrow_left.11911410.svg"
   },
   {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/github-pr-viewer/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
+    "url": "/github-pr-viewer/static/media/Roboto-Medium.894a2ede.ttf"
+  },
+  {
     "revision": "f5bdd04102ef2b25048e",
     "url": "/github-pr-viewer/static/js/1.f5bdd041.chunk.js"
   },
@@ -124,7 +124,7 @@ self.__precacheManifest = [
     "url": "/github-pr-viewer/static/css/1.6fedd9ff.chunk.css"
   },
   {
-    "revision": "63f388faa9b832a3a31015b725c64507",
+    "revision": "55b6ed4862be18c19c082b00bbdddb35",
     "url": "/github-pr-viewer/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/github-pr-viewer/precache-manifest.5a2edfca1f6c0d1ec11c0ae993b451d5.js"
+  "/github-pr-viewer/precache-manifest.7705b29b4b5c46f6122a1d68f6b6745f.js"
 );
 
 workbox.clientsClaim();
